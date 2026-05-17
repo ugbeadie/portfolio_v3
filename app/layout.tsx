38eeb9d3152afdf4683323b10150ba929d77f5e1
ugbeadie <ugbeadie3@gmail.com>
2026-05-17 19:59:57 +0100

    chore: set default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </body>
